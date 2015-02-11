6a25dfcddd6f36ce5663cb3cc23736b3e1cc970a
cjblanch <chad.j.blanchard@gmail.com>
2015-02-11 13:31:46 -0500

    First commit!
    Added code blocks project moonlight to repo

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x = 0;
+   cout << "Enter a number: ";
+   cin >> x;
+   cout << "You entered: " << x << endl;
+
+   return 0;
+}
